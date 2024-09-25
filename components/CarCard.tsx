38fd66512a2e606eb3f9903fd3752b853a1fc909
setu <setulalchowhan@gmail.com>
2024-09-25 19:52:30 +0600

    fixed some issue

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,11 +13,11 @@ interface CarDetails{
 
 const CarCard = ({id,image,title,link,details}:CarDetails) => {
   return (
-    <div className= ' flex flex-col justify-start items-start gap-4  p-10 border-[1px] border-gray-800 '>
+    <div className= ' flex flex-col justify-start items-start px-2 lg:px-3   space-y-3   border-[1px] border-gray-800 '>
         <Image src={image} alt='car' />
         <h1 className='text-[24px] font-[500]'>{title}</h1>
-        <p className='text-[20px] bg-[#262626] px-4 rounded-lg '>{link}</p>
-        <p className=' text-[20px] text-[#98989A] text-justify'>{details}</p>
+        <p className='text-[20px] bg-[#262626] lg:px-4 rounded-lg p-2  '>{link}</p>
+        <p className=' text-[20px] text-[#98989A] md:text-justify '>{details}</p>
     </div>
   )
 }
--- components/ProcessCard.tsx
+++ components/ProcessCard.tsx
@@ -9,7 +9,7 @@ interface Work{
 
 const ProcessCard = ({id,title,des}:Work) => {
   return (
-    <div className='  mx-auto lg:px-14    border-2  border-gray-800 rounded-lg shadow-lg p-10'>
+    <div className='  mx-auto lg:px-14    border-2  border-gray-800 rounded-lg shadow-lg md:p-10 p-3'>
       <div className='flex items-center mb-4'>
         <h1 className='md:text-[150px] font-[600] text-[#8F94FB] leading-none sm:text-[100px] text-[80px] '>0{id}</h1>
         <div className='ml-4'>
